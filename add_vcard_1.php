f052ab9f5ee2a4349b38ce4316a4b7439ddd7c4e
Rajender <rthakur.dev@gmail.com>
2021-02-01 18:43:59 +0530

    Basic layout create for test project

---
+++ add_vcard_1.php
@@ -0,0 +1,13 @@
+<?php include('header.php'); ?>
+  <!-- Begin Page Content -->
+  <div class="container-fluid">
+    
+    <form class="" action="index.html" method="post">
+        
+        <input type="text" class="form-class" name="" value="">
+      
+    </form>
+
+
+  </div>
+<?php include('footer.php'); ?>
---
+++ index.php
@@ -0,0 +1,31 @@
+<?php include('header.php'); ?>
+
+                <!-- Begin Page Content -->
+                <div class="container-fluid">
+                    
+                    <div class="row mb-4">
+                      <div class="col-md-12 text-right">
+                          <a href="add_vcard_1.php" class="btn btn-primary">Add Vcard</a>
+                      </div>
+                    </div>
+                    <div class="row">
+                      
+                      <div class="col-md-12">
+                            <div class="card">
+                              <div class="card-body">
+                                <h5 class="card-title">Business 1</h5>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                              </div>
+                              <div class="card-footer text-right">
+                                <a href="#" class="btn btn-primary btn-sm">Edit</a>
+                                <a href="#" class="btn btn-danger btn-sm">Delete</a>
+                              </div>  
+                            </div>
+                      </div>
+                      
+                      
+                    </div>                 
+                              
+                </div>
+
+<?php include('footer.php'); ?>
